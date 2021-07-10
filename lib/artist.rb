ea369d372d60215944a5c6d5d58810bdae14b7f4
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-10 18:31:37 -0400

    Completed assignment

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,44 @@
+require 'pry'
 
+class Artist
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+
+    def self.all
+        @@all
+    end
+
+
+    def songs
+        # returns all songs associated with this Artist
+        Song.all.select do |song|
+        song.artist == self
+        # binding.pry
+        end
+    end
+
+
+    def new_song(name, genre)
+        # given a name and genre, creates a new song associated with that artist
+        song = Song.new(name, self, genre)
+        # binding.pry
+    end
+
+
+    def genres
+        # has many genres, through songs
+        songs.map do |song|
+            song.genre
+            # binding.pry
+        end
+    end
+
+
+end
--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,43 @@
+require 'pry'
 
+class Doctor
+    attr_accessor :name
+
+    @@all = []
+
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+
+    def self.all
+        @@all
+    end
+
+
+    def new_appointment(date, patient)
+        # given a date and a patient, creates a new appointment
+        appointment = Appointment.new(date, patient, self)
+    end
+
+
+    def appointments
+        # returns all appointments associated with this Doctor
+        Appointment.all.select do |appointment|
+            appointment.doctor == self
+            # binding.pry
+        end
+    end
+
+
+    def patients
+        # has many patients, through appointments
+        appointments.map do |appointment|
+            appointment.patient
+            # binding.pry
+        end
+    end
+
+end
