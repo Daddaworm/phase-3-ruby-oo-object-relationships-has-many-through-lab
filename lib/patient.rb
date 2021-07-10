622ebf6b5fee0957b7a43ba41d7a3fa5b6dab9e8
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-10 18:30:29 -0400

    commit tree a0e5d2481e642cd8c660bca6770e1671d56c9828

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,41 @@
 
+class Patient
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+
+    def self.all
+        @@all
+    end
+
+    
+    def new_appointment(date, doctor)
+        # given a date and doctor, creates a new appointment belonging to that patient
+        appointment = Appointment.new(date, self, doctor)
+    end
+
+
+    def appointments
+        # returns all appointments associated with this Patient
+        Appointment.all.select do |appointment|
+            appointment.patient == self
+            # binding.pry
+        end
+    end
+
+
+    def doctors
+        # has many doctors through appointments
+        appointments.map do |appointment|
+            appointment.doctor
+        end
+    end
+
+
+end
